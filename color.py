5b7773bec2edcb9e9c7c6051d77978ffcea28612
ZhuohaoTan <80125456+ZhuohaoTan@users.noreply.github.com>
2021-05-06 21:59:01 -0400

    Add files via upload
    The code is revised following the YouTube tutorial: https://www.youtube.com/watch?v=b4XP2IcI-Bg

---
+++ color.py
@@ -0,0 +1,52 @@
+GRID_COLOR = "#BBADA0"
+EMPTY_CELL_COLOR = "#CDC1B4"
+SCORE_LABEL_FONT = ("Verdana", 24)
+SCORE_FONT = ("Helvetica", 48, "bold")
+GAME_OVER_FONT = ("Helvetica", 48, "bold")
+GAME_OVER_FONT_COLOR = "#ffffff"
+WINNER_BG = "#ffcc00"
+LOSER_BG = "#a39489"
+
+CELL_COLORS = {
+    2: "#EEE4DA",
+    4: "#EEE1C9",
+    8: "#F3B27A",
+    16: "#F69664",
+    32: "#F77C5F",
+    64: "#F75F3B",
+    128: "#EDD073",
+    256: "#EDCC62",
+    512: "#ffdb4a",
+    1024: "#f0b922",
+    2048: "fad74d"
+}
+
+CELL_NUMBER_COLORS = {
+    2: "#685c57",
+    4: "#695c57",
+    8: "#ffffff",
+    16: "#ffffff",
+    32: "#ffffff",
+    64: "#ffffff",
+    128: "#ffffff",
+    256: "#ffffff",
+    512: "#ffffff",
+    1024: "#ffffff",
+    2048: "#ffffff"
+}
+
+CELL_NUMBER_FONTS = {
+
+    2: ("Helvetica", 55, "bold"),
+    4:  ("Helvetica", 55, "bold"),
+    8:  ("Helvetica", 55, "bold"),
+    16:  ("Helvetica", 50, "bold"),
+    32:  ("Helvetica", 50, "bold"),
+    64: ("Helvetica", 50, "bold"),
+    128: ("Helvetica", 45, "bold"),
+    256: ("Helvetica", 45, "bold"),
+    512: ("Helvetica", 45, "bold"),
+    1024: ("Helvetica", 40, "bold"),
+    2048: ("Helvetica", 40, "bold")
+
+}
